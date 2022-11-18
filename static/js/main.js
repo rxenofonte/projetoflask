9f30151ff76de95ef6faa23a698ef47733fa05e6
Rommel Xenofonte Teles <rxenofonte@gmail.com>
2022-11-18 13:05:19 -0300

    Ajustes

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,32 @@ $(document).ready(function(){
     $('.modal').modal();
 });
 
+$('.chips-placeholder').chips({
+    placeholder: '+Tag',
+    secondaryPlaceholder: '+Tag',
+});
 
+$('.chips-autocomplete').chips({
+    autocompleteOptions: {
+    data: {
+        'Família': null,
+        'Trabalho': null,
+        'Amigos': null,
+        'Outros': null
+    },
+    limit: Infinity,
+    minLength: 1
+    }
+});
+
+var chip = {
+    tag: 'chip content',
+    image: '', //optional
+};
+
+var instance = M.Chips.getInstance(document.getElementById('chip1'))
+
+instance.addChip({
+    tag: 'Família',
+    image: '', // optional
+});
